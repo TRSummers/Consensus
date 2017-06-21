4ae9fd7f149c681f9dfc342e4e04da60684740cf
Uday Jarajapu <uday.jarajapu@consensuscorp.com>
2017-06-21 16:09:10 -0700

    removed commented block and auto-indented lines for choose path module

--- user-files/simulations/Common.scala
+++ user-files/simulations/Common.scala
@@ -298,126 +298,106 @@ object Common{
 			.headers(headers_14))
 		
 	}
-//	val CommonPause=pause(5,10)
-//
-//	val SA=group("SA"){
-//		// SA
-//		exec(http("SalesandActivations16")
-//			.get("/retail/orderassembly/pickyourpath.htm")
-//			.check(regex("blank'>(.+?)<").saveAs("p_sessionid"))
-//			.headers(headers_16))
-//		.exec(http("SalesandActivations17")
-//			.get("/retail/public/styles/normalize.php")
-//			.headers(headers_17))
-//		.exec(http("SalesandActivations18")
-//			.get("/jslibs/modernizr.php")
-//			.headers(headers_18))
-//		.exec(http("SalesandActivations19")
-//			.get("/js/retail/topnav.php")
-//			.headers(headers_18))
-//		.pause(120 milliseconds)
-//		.exec(http("SalesandActivations20")
-//			.get("/js/retail/getactivealerts.php?reqType=getactivealerts&cacheVar=1489013206068")
-//			.headers(headers_20))
-//	}
-		val RetailToChoosePathModule=group("RetailToChoosePathModule"){
-			exec(http("Retail_0")
-        .post("/retail/")
-        .headers(headers_0)
-        //.formParam("redirectUrl", "https%3A%2F%2Fperf-scale-ui.consensuscorpdev.com%2Fshopping%2F")
-        .formParam("userAction", "shopping")
-        .resources(http("request_1")
-            .get(uri_ui + "/build/css_d4c30075bc2eb1a8b7a8_min.js")
-            .headers(ui_headers_1),
+	val CommonPause=pause(5,10)
+
+  val RetailToChoosePathModule=group("RetailToChoosePathModule"){
+    exec(http("Retail_0")
+      .post("/retail/")
+      .headers(headers_0)
+      //.formParam("redirectUrl", "https%3A%2F%2Fperf-scale-ui.consensuscorpdev.com%2Fshopping%2F")
+      .formParam("userAction", "shopping")
+      .resources(http("request_1")
+        .get(uri_ui + "/build/css_d4c30075bc2eb1a8b7a8_min.js")
+        .headers(ui_headers_1),
         http("request_2")
-            .get(uri_ui + "/build/bundle_d4c30075bc2eb1a8b7a8_min.js")
-            .headers(ui_headers_1),
+          .get(uri_ui + "/build/bundle_d4c30075bc2eb1a8b7a8_min.js")
+          .headers(ui_headers_1),
         http("ui_request_3_fonts")
-            .get(uri5_google + "?family=Lato:400,700,400italic,700italic&subset=latin")
-            .headers(ui_headers_1),
+          .get(uri5_google + "?family=Lato:400,700,400italic,700italic&subset=latin")
+          .headers(ui_headers_1),
         http("ui_request_4_gif")
-            .get(uri_ui + "/assets/img/cloader.gif")
-            .headers(ui_headers_1),
+          .get(uri_ui + "/assets/img/cloader.gif")
+          .headers(ui_headers_1),
         http("request_5")
-            .get(uri4_gstatic + "/MDadn8DQ_3oT6kvnUq_2r_esZW2xOQ-xsNqO47m55DA.woff2")
-            .headers(ui_headers_5),
+          .get(uri4_gstatic + "/MDadn8DQ_3oT6kvnUq_2r_esZW2xOQ-xsNqO47m55DA.woff2")
+          .headers(ui_headers_5),
         http("request_6")
-            .get(uri_ui + "/config.json")
-            .headers(ui_headers_6),
+          .get(uri_ui + "/config.json")
+          .headers(ui_headers_6),
         http("dsom_request_7_options")
-            .options(uri_dsom_v1 + "/getContentForAisle")
-            .headers(dsom_headers_7),
+          .options(uri_dsom_v1 + "/getContentForAisle")
+          .headers(dsom_headers_7),
         http("dsom_request_8_options")
-            .options(uri_dsom_v1 + "/getNextState")
-            .headers(dsom_headers_7),
+          .options(uri_dsom_v1 + "/getNextState")
+          .headers(dsom_headers_7),
         http("dsom_request_9")
-            .post(uri_dsom_v1 + "/getNextState")
-            .headers(dsom_headers_9)
-            .body(ElFileBody("dsom/choosepath/dsom_start_options_request.json")),
+          .post(uri_dsom_v1 + "/getNextState")
+          .headers(dsom_headers_9)
+          .body(ElFileBody("dsom/choosepath/dsom_start_options_request.json")),
         http("dsom_request_10")
-            .post(uri_dsom_v1 + "/getContentForAisle")
-            .headers(dsom_headers_9)
-            .body(ElFileBody("dsom/choosepath/dsom_start_post_request.json")),
+          .post(uri_dsom_v1 + "/getContentForAisle")
+          .headers(dsom_headers_9)
+          .body(ElFileBody("dsom/choosepath/dsom_start_post_request.json")),
         http("poa_request_11_process_php")
-            .get(uri_poa + "/retail/orderassembly/controller/process.php")
-            .headers(poa_headers_11),
+          .get(uri_poa + "/retail/orderassembly/controller/process.php")
+          .headers(poa_headers_11),
         http("dsom_request_12")
-            .get(uri_ui + "/build/css_d4c30075bc2eb1a8b7a8_min.js"),
+          .get(uri_ui + "/build/css_d4c30075bc2eb1a8b7a8_min.js"),
         http("dsom_request_13")
-            .get(uri_ui + "/build/bundle_d4c30075bc2eb1a8b7a8_min.js"),
+          .get(uri_ui + "/build/bundle_d4c30075bc2eb1a8b7a8_min.js"),
         http("ui_request_14_google_api")
-            .get(uri5_google + "?family=Lato:400,700,400italic,700italic&subset=latin"),
+          .get(uri5_google + "?family=Lato:400,700,400italic,700italic&subset=latin"),
         http("ui_request_15_fonts")
-            .get(uri4_gstatic + "/MDadn8DQ_3oT6kvnUq_2r_esZW2xOQ-xsNqO47m55DA.woff2"),
+          .get(uri4_gstatic + "/MDadn8DQ_3oT6kvnUq_2r_esZW2xOQ-xsNqO47m55DA.woff2"),
         http("ui_request_16_gif")
-            .get(uri_ui + "/assets/img/cloader.gif"),
+          .get(uri_ui + "/assets/img/cloader.gif"),
         http("ui_request_17_config")
-            .get(uri_ui + "/config.json")
-            .headers(ui_headers_6),
+          .get(uri_ui + "/config.json")
+          .headers(ui_headers_6),
         http("dsom_request_18")
-            .post(uri_dsom_v1 + "/getContentForAisle")
-            .headers(dsom_headers_9)
-            .body(ElFileBody("dsom/choosepath/dsom_start_content_post_request.json")),
+          .post(uri_dsom_v1 + "/getContentForAisle")
+          .headers(dsom_headers_9)
+          .body(ElFileBody("dsom/choosepath/dsom_start_content_post_request.json")),
         http("dsom_request_19")
-            .post(uri_dsom_v1 + "/getNextState")
-            .headers(dsom_headers_9)
-            .body(ElFileBody("dsom/choosepath/dsom_start_getNextState_post_request.json")),
+          .post(uri_dsom_v1 + "/getNextState")
+          .headers(dsom_headers_9)
+          .body(ElFileBody("dsom/choosepath/dsom_start_getNextState_post_request.json")),
         http("dsom_request_20_path_content")
-            .post(uri_dsom_v1 + "/getContentForAisle")
-            .check(regex("Purchase unactivated device at full price").find.exists)
-            .headers(dsom_headers_9)
-            .body(ElFileBody("dsom/choosepath/dsom_start_content_frame_post_request.json")),
+          .post(uri_dsom_v1 + "/getContentForAisle")
+          .check(regex("Purchase unactivated device at full price").find.exists)
+          .headers(dsom_headers_9)
+          .body(ElFileBody("dsom/choosepath/dsom_start_content_frame_post_request.json")),
         http("dsom_request_21")
-            .get(uri_ui + "/build/ch_9afac72ed1aa9ce2cabc_min.js")
-            .headers(ui_headers_1),
+          .get(uri_ui + "/build/ch_9afac72ed1aa9ce2cabc_min.js")
+          .headers(ui_headers_1),
         http("dsom_request_22")
-            .get(uri_ui + "/app/pages/frame/header/header.html")
-            .headers(ui_headers_22),
+          .get(uri_ui + "/app/pages/frame/header/header.html")
+          .headers(ui_headers_22),
         http("ui_request_23")
-            .get(uri_ui + "/app/pages/frame/footer/footer.html")
-            .headers(ui_headers_22),
+          .get(uri_ui + "/app/pages/frame/footer/footer.html")
+          .headers(ui_headers_22),
         http("ui_request_24_choosepath")
-            .get(uri_ui + "/app/pages/choosepath/choosepath.html")
-            .headers(ui_headers_22),
+          .get(uri_ui + "/app/pages/choosepath/choosepath.html")
+          .headers(ui_headers_22),
         http("ui_request_25_optionscolumn")
-            .get(uri_ui + "/app/pages/choosepath/partials/optionscolumn.html")
-            .headers(ui_headers_22),
+          .get(uri_ui + "/app/pages/choosepath/partials/optionscolumn.html")
+          .headers(ui_headers_22),
         http("ui_request_26")
-            .get(uri_ui + "/build/font_af7ae505a9eed503f8b8e6982036873e.woff2")
-            .headers(ui_headers_5),
+          .get(uri_ui + "/build/font_af7ae505a9eed503f8b8e6982036873e.woff2")
+          .headers(ui_headers_5),
         http("ui_request_27_fonts")
-            .get(uri4_gstatic + "/MgNNr5y1C_tIEuLEmicLmwLUuEpTyoUstqEm5AMlJo4.woff2")
-            .headers(ui_headers_5),
+          .get(uri4_gstatic + "/MgNNr5y1C_tIEuLEmicLmwLUuEpTyoUstqEm5AMlJo4.woff2")
+          .headers(ui_headers_5),
         http("ui_request_28_svg")
-            .get(uri_ui + "/assets/img/bullseye.svg")
-            .headers(ui_headers_1),
+          .get(uri_ui + "/assets/img/bullseye.svg")
+          .headers(ui_headers_1),
         http("poa_request_29")
-            .get(uri_poa + "/img/brands/target/retail/sign_up_new_red.svg")
-            .headers(ui_headers_1),
+          .get(uri_poa + "/img/brands/target/retail/sign_up_new_red.svg")
+          .headers(ui_headers_1),
         http("poa_request_30")
-            .get(uri_poa + "/img/brands/target/retail/upgrade_new_red.svg")
-            .headers(ui_headers_1)))
-}
+          .get(uri_poa + "/img/brands/target/retail/upgrade_new_red.svg")
+          .headers(ui_headers_1)))
+  }
 
 
 	//pause(5, 10)
