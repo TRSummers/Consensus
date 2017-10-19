34e54373e78e24823d103fa3cbcac8bd2e39f2dc
tward <tim.r.ward@consensuscorp.com>
2017-10-18 22:18:13 -0700

    Verizon CR Expectations tested, Verizon Upgrade initial commit removed (bad script), minor name/path changes

---
+++ user-files/simulations/Sprint/SprintUpgradeCRComponents.scala
@@ -0,0 +1,41 @@
+import scala.io.Source
+import scala.concurrent.duration._
+import io.gatling.core.Predef._
+import io.gatling.http.Predef._
+import io.gatling.jdbc.Predef._
+import scala.util.Random
+
+object SprintUpgradeCRComponents {
+
+  val expectation1 = "expectations/sprint/upgrade/check-loan-eligibility"
+  val expectation2 = "expectations/sprint/upgrade/create-lite-esa"
+  val expectation3 = "expectations/sprint/upgrade/create-loan-contract"
+  val expectation4 = "expectations/sprint/upgrade/query-account-loan-details"
+  val expectation5 = "expectations/sprint/upgrade/submit-order"
+  val expectation6 = "expectations/sprint/upgrade/validate-account-info"
+  val expectation7 = "expectations/sprint/upgrade/account-validation"
+  val expectation8 = "expectations/sprint/upgrade/credit-check"
+  val expectation9 = "expectations/sprint/upgrade/service-validation"
+  val expectation10 = "expectations/sprint/upgrade/activation"
+
+  val gatlingData = sys.env("GATLING_DATA")
+  val expectationPath = Array(
+    Map("expectationPath" -> expectation1, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation1 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation2, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation2 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation3, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation3 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation4, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation4 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation5, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation5 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation6, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation6 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation7, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation7 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation8, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation8 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation9, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation9 + "-payload.xml").mkString)),
+    Map("expectationPath" -> expectation10, "payload" -> CRExpectationManager.escapePayload(Source.fromFile(gatlingData + "/" + expectation10 + "-payload.xml").mkString))
+  )
+
+  val SetUpExpectations = scenario("Sprint Upgrade Carrier Responder").repeat(10) {
+    group("Sprint Upgrade Carrier Responder") {
+      exec(feed(expectationPath))
+        .exec(CRExpectationManager.createAndLoadExpectation)
+    }
+  }
+}
