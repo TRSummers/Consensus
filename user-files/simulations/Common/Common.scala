5d155e3de02e043a07c33cbded67d06cfa9509c1
tward <tim.r.ward@consensuscorp.com>
2017-11-01 15:30:47 -0700

    changing common pause to (2,5) for debugging R14

--- user-files/simulations/Common/Common.scala
+++ user-files/simulations/Common/Common.scala
@@ -22,7 +22,8 @@ object Common{
 	val uri_gstatic = "https://fonts.gstatic.com/s/lato/v14"
 	val uri_google  = "https://fonts.googleapis.com/css"
 
-	val CommonPause=pause(8,45)
+//	val CommonPause=pause(8,45)
+	val CommonPause=pause(2,5)
 
 
 	val VZWCarrierTestData =Iterator.continually(
