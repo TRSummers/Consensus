169e5503c4461cdc69197a2b5d62062da0c37ae2
tward <tim.r.ward@consensuscorp.com>
2017-11-01 01:28:49 -0700

    compilation fix for ModularTest

--- user-files/simulations/Jenkins/ModularTest.scala
+++ user-files/simulations/Jenkins/ModularTest.scala
@@ -9,6 +9,7 @@ class ModularTest extends Simulation {
 	setUp(
 		SprintScenarios.Upgrade.inject(rampUsers(SimParams.SUG) over (SimParams.rampup seconds)).protocols(SimParams.httpProtocol),
 		VerizonScenarios.NewActivation.inject(rampUsers(SimParams.VNA) over (SimParams.rampup seconds)).protocols(SimParams.httpProtocol),
-		VerizonScenarios.Upgrade.inject(rampUsers(SimParams.VUG) over (SimParams.rampup seconds)).protocols(SimParams.httpProtocol)),
+		VerizonScenarios.Upgrade.inject(rampUsers(SimParams.VUG) over (SimParams.rampup seconds)).protocols(SimParams.httpProtocol),
 		ATTScenarios.Upgrade.inject(rampUsers(SimParams.TUG) over (SimParams.rampup seconds)).protocols(SimParams.httpProtocol)
+	)
 }
