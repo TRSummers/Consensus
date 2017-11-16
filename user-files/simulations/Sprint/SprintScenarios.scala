49210c98d80db12e75c88bf81c529e6a63a2a863
tsummers <thomas.summers@consensuscorp.com>
2017-11-16 15:03:59 -0800

    Update SprintScenarios.scala

--- user-files/simulations/Sprint/SprintScenarios.scala
+++ user-files/simulations/Sprint/SprintScenarios.scala
@@ -11,8 +11,28 @@ object SprintScenarios {
     group("SPT UPG") {
       exec(
         feed(Common.CreditCheckData),
-
-        Common.LoginToRetail, Common.CommonPause,
+                Common.LoginToRetail,              Common.CommonPause,
+                Common.RetailToChoosePathModule,   Common.CommonPause,
+                SPRLegacy3.Upgrade,Common.CommonPause,
+              SPRLegacy3.SelectSprint,Common.CommonPause,
+              SPRLegacy3.UEC,Common.CommonPause,
+              SPRLegacy3.UECResults,Common.CommonPause,
+              SPRLegacy3.Scan,Common.CommonPause,
+              SPRLegacy3.PO,Common.CommonPause,
+              SprintFlow.CC,Common.CommonPause,
+              SPRLegacy4.CCResult,Common.CommonPause,
+              SprintFlow.IDP2Plan,Common.CommonPause,
+              SPRLegacy4.Plan,Common.CommonPause,
+              SPRLegacy4.Cart,Common.CommonPause,
+              SPRLegacy4.Features,Common.CommonPause,
+              SPRLegacy4.ProtectionPlan,Common.CommonPause,
+              SPRLegacy4.OrderReview,Common.CommonPause,
+              SPRLegacy4.TC,Common.CommonPause,
+              SPRLegacy4.PMSS,Common.CommonPause,
+              SPRLegacy4.PV,Common.CommonPause,
+              SPRLegacy4.VACT,Common.CommonPause,
+              SPRLegacy4.DevFin,Common.CommonPause
+      /*  Common.LoginToRetail, Common.CommonPause,
         Common.RetailToChoosePathModule, Common.CommonPause,
         SprintUpgradePages.ChoosePathToUpgradeEligibilityCheck, Common.CommonPause,
         SprintUpgradePages.UpgradeEligibilityCheckToScanDevice, Common.CommonPause,
@@ -31,7 +51,7 @@ object SprintScenarios {
         SprintUpgradePages.ScanReceiptToActivationScan, Common.CommonPause,
         SprintUpgradePages.ActivationScanToWirelessCustomerAgreement_LEGACY, Common.CommonPause,
         SprintUpgradePages.WirelessCustomerAgreementToDeviceFinancingInstallmentContract_LEGACY, Common.CommonPause,
-        SprintUpgradePages.DeviceFinancingInstallmentContractToOrderSummary_LEGACY, Common.CommonPause
+        SprintUpgradePages.DeviceFinancingInstallmentContractToOrderSummary_LEGACY, Common.CommonPause*/
         //        SprintUpgradePages.TabulateResults, Common.CommonPause,
         //    Common.NewGuest, Common.CommonPause,
         //        Common.Logout
