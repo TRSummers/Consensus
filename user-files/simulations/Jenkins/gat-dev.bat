71ba4e1aa3de0165db261154535715671f7d388b
tward <Ready4Hillary>
2018-03-19 12:31:08 -0700

    changes to gat-dev.bat and modular test to allow for CR setup through the Jenkins job

--- user-files/simulations/Jenkins/gat-dev.bat
+++ user-files/simulations/Jenkins/gat-dev.bat
@@ -57,4 +57,4 @@ echo ******************************************
 
 set GATLING_DATA=%USERFILESLOCATION%\user-files\bodies
 
-c:/gatling/gatling-charts-highcharts-bundle-2.2.3/bin/gatling.bat -sf %USERFILESLOCATION%\user-files\simulations -bdf %USERFILESLOCATION%\user-files\bodies -bf %USERFILESLOCATION%\binaries -on VZW_%VNAUSERS%_%ITERATIONS%_Iterations -s CarrierResponder.SohoSetup
+c:/gatling/gatling-charts-highcharts-bundle-2.2.3/bin/gatling.bat -sf %USERFILESLOCATION%\user-files\simulations -bdf %USERFILESLOCATION%\user-files\bodies -bf %USERFILESLOCATION%\binaries -on VZW_%VNAUSERS%_%ITERATIONS%_Iterations -s CarrierResponder.ModularTest
