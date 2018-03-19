b28edd9fa91d77916b139b3cb34cf75eb442467b
tward <Ready4Hillary>
2018-03-19 12:40:32 -0700

    changes to gat-dev.bat and modular test to allow for CR setup through the Jenkins job

--- user-files/simulations/Jenkins/ModularTest.scala
+++ user-files/simulations/Jenkins/ModularTest.scala
@@ -18,8 +18,10 @@ import Sprint.Soho.SprintSohoCRComponents
 class ModularTest extends Simulation {
 	setUp(
 
+    // CLEAN CR
+    CRExpectationManager.CleanExpectations.inject(atOnceUsers(SimParams.TGTCR + SimParams.SOHOCR)).protocols(SimParams.httpProtocol),
+
     // TARGET CR
-    CRExpectationManager.CleanExpectations.inject(atOnceUsers(SimParams.TGTCR)).protocols(SimParams.httpProtocol),
     VerizonCRComponents.SetUpExpectations.inject(nothingFor(15 seconds), atOnceUsers(SimParams.TGTCR)).protocols(SimParams.httpProtocol),
     ATTCRComponents.SetUpExpectations.inject(nothingFor(45 seconds), atOnceUsers(SimParams.TGTCR)).protocols(SimParams.httpProtocol),
     SprintTargetCRComponents.SetUpExpectations.inject(nothingFor(75 seconds), atOnceUsers(SimParams.TGTCR)).protocols(SimParams.httpProtocol),
@@ -31,7 +33,6 @@ class ModularTest extends Simulation {
 		ATTScenarios.Upgrade.inject(rampUsers(SimParams.TGTTUG) over (SimParams.rampup seconds)).protocols(SimParams.httpProtocol),
 
     // SOHO CR
-    CRExpectationManager.CleanExpectations.inject(atOnceUsers(SimParams.SOHOCR)).protocols(SimParams.SohoHttpProtocol),
     SprintSohoCRComponents.SetUpExpectations.inject(nothingFor(15 seconds), atOnceUsers(SimParams.SOHOCR)).protocols(SimParams.SohoHttpProtocol),
 
     // SOHO
